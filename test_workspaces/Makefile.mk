b78eaafe2baddfdb5db4b19f2096b7de347d3d5c
Adam Liddell <git@aliddell.com>
2019-08-08 20:34:00 +0100

    Add test workspace that uses both strip_import_prefix and import_prefix

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -2,6 +2,10 @@ test_workspace_absolute_strip_import_prefix:
 	cd test_workspaces/absolute_strip_import_prefix; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
+test_workspace_combined_strip_and_add_prefix:
+	cd test_workspaces/combined_strip_and_add_prefix; \
+	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 test_workspace_exclusions:
 	cd test_workspaces/exclusions; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
@@ -50,4 +54,4 @@ test_workspace_shared_proto:
 	cd test_workspaces/shared_proto; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
-all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_import_prefix test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto
+all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_combined_strip_and_add_prefix test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_import_prefix test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto
