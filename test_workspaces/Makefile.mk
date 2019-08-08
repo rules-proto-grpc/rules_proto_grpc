6a515f2c621d69f679218d0bb28340cc60ab0acf
Adam Liddell <git@aliddell.com>
2019-08-08 20:30:09 +0100

    Add test workspace for import_prefix

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -14,6 +14,10 @@ test_workspace_go_importpath:
 	cd test_workspaces/go_importpath; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
+test_workspace_import_prefix:
+	cd test_workspaces/import_prefix; \
+	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 test_workspace_objc_capitalisation:
 	cd test_workspaces/objc_capitalisation; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
@@ -46,4 +50,4 @@ test_workspace_shared_proto:
 	cd test_workspaces/shared_proto; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
-all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto
+all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_import_prefix test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto
