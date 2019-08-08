ee90ce5cb53a22ee19426e6f7e26c62b6e187927
Adam Liddell <git@aliddell.com>
2019-08-08 20:10:49 +0100

    Split strip_import_prefix test workspace to absolute and relative versions

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -1,3 +1,7 @@
+test_workspace_absolute_strip_import_prefix:
+	cd test_workspaces/absolute_strip_import_prefix; \
+	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 test_workspace_exclusions:
 	cd test_workspaces/exclusions; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
@@ -34,12 +38,12 @@ test_workspace_readme_http_archive:
 	cd test_workspaces/readme_http_archive; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
+test_workspace_relative_strip_import_prefix:
+	cd test_workspaces/relative_strip_import_prefix; \
+	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 test_workspace_shared_proto:
 	cd test_workspaces/shared_proto; \
 	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
-test_workspace_strip_import_prefix:
-	cd test_workspaces/strip_import_prefix; \
-	bazel --batch test --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
-
-all_test_workspaces: test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_shared_proto test_workspace_strip_import_prefix
+all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_objc_capitalisation test_workspace_python2_grpc test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto
