16daa3502e505bed688bd3e3599bbc1f48ad1a64
Daniel P. Purkhus <purkhusid@gmail.com>
2021-04-19 11:42:51 +0000

    Move plugin into workspace

--- fsharp/wrapper.sh
+++ fsharp/wrapper.sh
@@ -10,5 +10,5 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
 # --- end runfiles.bash initialization v2 ---
-GENERATOR="$(rlocation protoc-gen-fsharp/bin/FSharp.GrpcCodeGenerator.dll_0.exe)"
+GENERATOR="$(rlocation rules_proto_grpc/fsharp/protoc-gen-fsharp/FSharp.GrpcCodeGenerator.dll_0.exe)"
 $GENERATOR $@
