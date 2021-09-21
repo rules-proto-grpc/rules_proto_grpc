30abb143935a263da9efd2d6783114ade0a0d8aa
Adam Liddell <git@aliddell.com>
2021-09-21 23:34:38 +0100

    Add possible workaround for OSX failures

--- tools/rulegen/main.go
+++ tools/rulegen/main.go
@@ -475,6 +475,7 @@ func mustWriteBazelciPresubmitYml(dir string, languages []*Language, envVars []s
 		out.w("    build_flags:")
 		if ciPlatform == "macos" {
 			out.w(`    - "--copt=-DGRPC_BAZEL_BUILD"`) // https://github.com/bazelbuild/bazel/issues/4341 required for macos
+			out.w(`    - "--features=-supports_dynamic_linker`)  // TODO: Needed until Bazel 5.0.0: https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758361769
 		}
 		for _, flag := range ciPlatformFlags[ciPlatform] {
 			out.w(`    - "%s"`, flag)
@@ -490,6 +491,7 @@ func mustWriteBazelciPresubmitYml(dir string, languages []*Language, envVars []s
 		out.w(`    - "--test_output=errors"`)
 		if ciPlatform == "macos" {
 			out.w(`    - "--copt=-DGRPC_BAZEL_BUILD"`) // https://github.com/bazelbuild/bazel/issues/4341 required for macos
+			out.w(`    - "--features=-supports_dynamic_linker`)  // TODO: Needed until Bazel 5.0.0: https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758361769
 		}
 		for _, flag := range ciPlatformFlags[ciPlatform] {
 			out.w(`    - "%s"`, flag)
@@ -522,6 +524,7 @@ func mustWriteBazelciPresubmitYml(dir string, languages []*Language, envVars []s
 				out.w("    build_flags:")
 				if ciPlatform == "macos" {
 					out.w(`    - "--copt=-DGRPC_BAZEL_BUILD"`) // https://github.com/bazelbuild/bazel/issues/4341 required for macos
+					out.w(`    - "--features=-supports_dynamic_linker`)  // TODO: Needed until Bazel 5.0.0: https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758361769
 				}
 				if lang.Name == "csharp" || lang.Name == "fsharp" { // https://github.com/bazelbuild/rules_dotnet/issues/225
 					for _, flag := range ciPlatformFlags[ciPlatform] {
@@ -561,11 +564,13 @@ func mustWriteBazelciPresubmitYml(dir string, languages []*Language, envVars []s
 			out.w("    build_flags:")
 			if ciPlatform == "macos" {
 				out.w(`    - "--copt=-DGRPC_BAZEL_BUILD"`) // https://github.com/bazelbuild/bazel/issues/4341 required for macos
+				out.w(`    - "--features=-supports_dynamic_linker`)  // TODO: Needed until Bazel 5.0.0: https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758361769
 			}
 			out.w("    test_flags:")
 			out.w(`    - "--test_output=errors"`)
 			if ciPlatform == "macos" {
 				out.w(`    - "--copt=-DGRPC_BAZEL_BUILD"`) // https://github.com/bazelbuild/bazel/issues/4341 required for macos
+				out.w(`    - "--features=-supports_dynamic_linker`)  // TODO: Needed until Bazel 5.0.0: https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758361769
 			}
 			out.w("    test_targets:")
 			out.w(`      - "//..."`)
