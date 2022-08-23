b66ebdb1faa8e52d2d9e58cd276a9cde463deb2c
Adam Liddell <git@aliddell.com>
2022-08-23 23:08:23 +0100

    Add test workspace for output_directory file copying #213

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -43,6 +43,11 @@ test_workspace_import_prefix:
 	cd test_workspaces/import_prefix; \
 	bazel --batch test --host_cxxopt=-std=c++17 ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
+.PHONY: test_workspace_nested_output_directory
+test_workspace_nested_output_directory:
+	cd test_workspaces/nested_output_directory; \
+	bazel --batch test --host_cxxopt=-std=c++17 ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 .PHONY: test_workspace_objc_capitalisation
 test_workspace_objc_capitalisation:
 	cd test_workspaces/objc_capitalisation; \
@@ -84,4 +89,4 @@ test_workspace_typescript:
 	bazel --batch test --host_cxxopt=-std=c++17 ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
 .PHONY: all_test_workspaces
-all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_combined_strip_and_add_prefix test_workspace_common_cpp_library test_workspace_empty_output_directory test_workspace_exclusions test_workspace_generated_proto test_workspace_go_fixer test_workspace_go_importpath test_workspace_import_prefix test_workspace_objc_capitalisation test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto test_workspace_typescript
+all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_combined_strip_and_add_prefix test_workspace_common_cpp_library test_workspace_empty_output_directory test_workspace_exclusions test_workspace_generated_proto test_workspace_go_fixer test_workspace_go_importpath test_workspace_import_prefix test_workspace_nested_output_directory test_workspace_objc_capitalisation test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_readme_http_archive test_workspace_relative_strip_import_prefix test_workspace_shared_proto test_workspace_typescript
