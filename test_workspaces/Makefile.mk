895ce09767ca714454e18d56c46042dc901f8661
Adam Liddell <git@aliddell.com>
2023-12-06 16:52:07 +0000

    Add go_fixer test workspace

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -23,6 +23,11 @@ test_workspace_generated_proto:
 	cd test_workspaces/generated_proto; \
 	bazel --batch test --enable_bzlmod ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
+.PHONY: test_workspace_go_fixer
+test_workspace_go_fixer:
+	cd test_workspaces/go_fixer; \
+	bazel --batch test --enable_bzlmod ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
+
 .PHONY: test_workspace_go_importpath
 test_workspace_go_importpath:
 	cd test_workspaces/go_importpath; \
@@ -69,4 +74,4 @@ test_workspace_special_characters:
 	bazel --batch test --enable_bzlmod ${BAZEL_EXTRA_FLAGS} --verbose_failures --disk_cache=../bazel-disk-cache --test_output=errors //...
 
 .PHONY: all_test_workspaces
-all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_combined_strip_and_add_prefix test_workspace_common_cpp_library test_workspace_exclusions test_workspace_generated_proto test_workspace_go_importpath test_workspace_import_prefix test_workspace_nested_output_directory test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_relative_strip_import_prefix test_workspace_shared_proto test_workspace_special_characters
+all_test_workspaces: test_workspace_absolute_strip_import_prefix test_workspace_combined_strip_and_add_prefix test_workspace_common_cpp_library test_workspace_exclusions test_workspace_generated_proto test_workspace_go_fixer test_workspace_go_importpath test_workspace_import_prefix test_workspace_nested_output_directory test_workspace_python3_grpc test_workspace_python_dashes test_workspace_python_deps test_workspace_relative_strip_import_prefix test_workspace_shared_proto test_workspace_special_characters
