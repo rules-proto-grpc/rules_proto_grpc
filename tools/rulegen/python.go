0ca5fe24844d5fb0dfcd449594c85e45a73605da
Zhongpeng Lin <zplin@uber.com>
2024-01-18 00:49:52 +0000

    quotes

--- tools/rulegen/python.go
+++ tools/rulegen/python.go
@@ -6,14 +6,14 @@ load("@rules_python//python:defs.bzl", "py_library")
 load("//:{{ .Lang.Name }}_{{ .Rule.Kind }}_compile.bzl", "{{ .Lang.Name }}_{{ .Rule.Kind }}_compile")
 
 def {{ .Rule.Name }}(name, **kwargs):
-    '''
+    """
     python_proto_library generates Python code from proto and creates a py_library for them.
 
     Args:
         name: the name of the target.
         **kwargs: common Bazel attributes will be passed to both python_proto_compile and py_library;
         python_proto_compile attributes will be passed to python_proto_compile only.
-    '''
+    """
 
     # Compile protos
     name_pb = name + "_pb"
@@ -49,14 +49,14 @@ load("@rules_python//python:defs.bzl", "py_library")
 load("//:{{ .Lang.Name }}_{{ .Rule.Kind }}_compile.bzl", "{{ .Lang.Name }}_{{ .Rule.Kind }}_compile")
 
 def {{ .Rule.Name }}(name, **kwargs):
-    '''
+    """
     python_grpc_library generates Python code from proto and gRPC, and creates a py_library for them.
 
     Args:
         name: the name of the target.
         **kwargs: common Bazel attributes will be passed to both python_grpc_compile and py_library;
         python_grpc_compile attributes will be passed to python_grpc_compile only.
-    '''
+    """
 
     # Compile protos
     name_pb = name + "_pb"
