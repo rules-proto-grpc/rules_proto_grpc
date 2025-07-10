c738330b98ef0c4599f3682ad2bfa498a409e8dd
Adam Liddell <git@aliddell.com>
2025-07-10 10:37:01 +0100

    Update rules_python to 1.5.1

--- tools/rulegen/python.go
+++ tools/rulegen/python.go
@@ -123,7 +123,7 @@ GRPCLIB_DEPS = [
     Label("@protobuf//:protobuf_python"),
 ]`)
 
-var pythonModuleSuffixLines = `bazel_dep(name = "rules_python", version = "1.5.0")
+var pythonModuleSuffixLines = `bazel_dep(name = "rules_python", version = "1.5.1")
 
 python = use_extension("@rules_python//python/extensions:python.bzl", "python")
 python.toolchain(python_version = "3.11")`
