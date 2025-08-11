35ec40dbfc09c7b5e7b5ef869dfe30c7260b3e33
Adam Liddell <git@aliddell.com>
2025-08-11 13:11:52 +0100

    Increase complexity of nested output dir test workspace, now the C# does not use output_directory

--- test_workspaces/nested_output_directory/main.py
+++ test_workspaces/nested_output_directory/main.py
@@ -4,9 +4,9 @@ import pathlib
 import sys
 
 expected = {
-    'mixed_lib/directory/service_pb2_grpc.py',
-    'mixed_lib/directory/service_grpc_pb.js',
-    'mixed_lib/ServiceGrpc.cs',
+    'mixed_lib/test/directory/service_pb2_grpc.py',
+    'mixed_lib/test/directory/service_grpc_pb.js',
+    'mixed_lib/test/ServiceGrpc.cs',
 }
 
 if os.name == 'nt':
