ce83039aecd679f0e197bca9e9fdc707911a5636
Adam Liddell <git@aliddell.com>
2025-08-21 10:23:36 +0100

    Update swift deps

--- modules/swift/Package.swift
+++ modules/swift/Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "rules_proto_grpc",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf", from: "1.30.0"),
+        .package(url: "https://github.com/apple/swift-protobuf", from: "1.31.0"),
         .package(url: "https://github.com/grpc/grpc-swift-2", from: "2.1.0"),
-        .package(url: "https://github.com/grpc/grpc-swift-protobuf", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf", from: "2.1.0"),
     ]
 )
