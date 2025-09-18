e07206cd8514d149ac48bce6bafac71853272bdd
Adam Liddell <git@aliddell.com>
2025-09-18 10:51:54 +0100

    Update swift-protobuf to 1.31.1

--- modules/swift/Package.swift
+++ modules/swift/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "rules_proto_grpc",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf", exact: "1.31.0"),
+        .package(url: "https://github.com/apple/swift-protobuf", exact: "1.31.1"),
         .package(url: "https://github.com/grpc/grpc-swift-2", exact: "2.1.0"),
         .package(url: "https://github.com/grpc/grpc-swift-protobuf", exact: "2.1.1"),
     ]
